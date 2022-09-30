71f50ba189cb2ee52f2d068384a4b66565cbd2b4
williskum <william.johansson1999@outlook.com>
2022-09-30 17:53:58 +0200

    Complete responsive

---
+++ resources/views/welcome.blade.php
@@ -0,0 +1,229 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>Sticky CSS Responsive</title>
+    <link rel="stylesheet" href="../../public/css/style.css" >
+    <link rel="stylesheet" href="/css/style.css">
+    <link href='https://fonts.googleapis.com/css?family=Poppins:500,600,700' rel='stylesheet'>
+</head>
+<body>
+    <div class="home">
+        <div class="background1">
+            <div class="sticky">
+                <div class="logo">
+                    <img src="/Images/LogoHeader.svg">
+                </div>
+                <input type="checkbox" id="nav-toggle" class="nav-toggle">
+                <div class="nav">
+                    <li><a href="#">Våra kläder</a></li>
+                    <li><a href="#">Outlet</a></li>
+                    <li><a href="#">Butiker</a></li>
+                    <li><a href="#">Kundservice</a></li>
+                    <li><a href="#">Om oss</a></li>
+                </div>
+                <div class="toggle-container">
+                    <label for="nav-toggle" class="nav-toggle-label">
+                        <span></span>
+                    </label>
+                </div>
+                <div class="button-container">
+                    <button class="button">Get in touch</button>
+                </div>
+            </div>
+            <div class="hero">
+                <div class="title">
+                    Så kliv in i Donkeys magiska värld – där barn alltid är barn.
+                </div>
+                <div class="textblock">
+                    <div class="text">
+                        Donkey har kläder till barn, från nyfödd och baby, till barn i förskole- och skolåldern. Barnkläder och bebiskläder som underlättar vardagen och som fungerar lika bra för fartfyllda utomhusaktiviteter som till fest.
+                    </div>
+                    <div>
+                        <button class="button">Våra kläder</button>
+                    </div>
+                </div>
+            </div>
+        </div>
+        <div class="content">
+            <div class="contentblock-left">
+                <div class="square">
+                    <div class="textblock">
+                        <div class="tiny-title">
+                            HÖSTKLÄDER
+                        </div>
+                        <div class="title">
+                            Härliga höstnyheter
+                        </div>
+                        <div class="text">
+                            Lorem ipsum dolor sit amet, consectetur adipiscing elit. Donec at sem arcu. Orci varius natoque penatibus et magnis dis parturient montes, nascetur ridiculus mus.
+                        </div>
+                        <div class="button-container">
+                            <button class="button">Shoppa nu</button>
+                        </div>
+                    </div>
+                </div>
+                <div class="image-container">
+                    <img src="/Images/IMG.jpg" class="image"> 
+                </div>
+            </div>
+            <div class="contentblock-right">
+                <div>
+                    <img src="/Images/IMG(1).jpg" class="image">
+                </div>
+                <div class="square">
+                    <div class="textblock">
+                        <div class="tiny-title">
+                            VÅRT SORTIMENT
+                        </div>
+                        <div class="title">
+                            Hållbara plagg
+                        </div>
+                        <div class="text">
+                            Med 100 % hållbarhetsmärkt bomull i hela vår kollektion gör vi kläder till baby och barn, men också kläder till den nyfödda familjemedlemmen.
+                        </div>
+                        <div class="button-container">
+                            <button class="button">Läs mer</button>
+                        </div>
+                    </div>
+                </div>
+            </div>
+            <div class="formblock">
+                <div class="textblock">
+                    <div class="tiny-title">
+                        STÄLL EN FRÅGA
+                    </div>
+                    <div class="title">
+                        Har du något du funderar över?
+                    </div>
+                    <div class="text">
+                        Med 100 % hållbarhetsmärkt bomull i hela vår kollektion gör vi kläder till baby och barn, men också kläder till den nyfödda familjemedlemmen.
+                    </div>
+                </div>
+                <div class="nameblock">
+                    <div class="title">
+                        Namn
+                    </div>
+                    <div class="form">
+                        <input type="text" placeholder="Skriv ditt namn" class="input">
+                    </div>
+                </div>
+                <div class="line1">
+                </div>
+                <div class="mailblock">
+                    <div class="title">
+                        Email
+                    </div>
+                    <div class="form">
+                        <input type="text" placeholder="Fyll i din mailadress" class="input">
+                    </div>
+                </div>
+                <div class="line2">
+                </div>
+                <div class="categoryblock">
+                    <div class="title">
+                        Välj underkategori
+                    </div>
+                    <div class="form">
+                        <label>
+                            <input type="radio" name="category" id="kategori1" class="radio">
+                            <div class="choice">Kategori 1</div>
+                        </label>
+                        <label>
+                            <input type="radio" name="category" id="kategori2" class="radio">
+                            <div class="choice">Kategori 2</div>
+                        </label>
+                        <label>
+                            <input type="radio" name="category" id="kategori3" class="radio">
+                            <div class="choice">Kategori 3</div>
+                        </label>
+                        <label>
+                            <input type="radio" name="category" id="kategori4" class="radio">
+                            <div class="choice">Kategori 4</div>
+                        </label>
+                    </div>
+                </div>
+                <div class="line3">
+                </div>
+                <div class="buttons">
+                    <button class="clear">Rensa alternativ</button>
+                    <button class="send">Skicka</button>
+                </div>
+            </div>
+        </div>
+        <div class="background2">
+            <div class="footer">
+                <div class="image">
+                    <div class="textblock">
+                        <div class="title">
+                            Förändringen börjar nu!
+                        </div>
+                        <div class="text">
+                            Vi kommer alltid att utgå ifrån barnens bästa och vår hållbarhetsvision är att göra barnklädesmarknaden mer hållbar – för jorden går i arv, och det ska våra kläder också göra.
+                        </div>
+                        <div>
+                            <button class="button">Vårt hållbarhetslöfte</button>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+        <div class="background3">
+            <div class="bottom">
+                <div class="logo">
+                    <img src="/Images/LogoFooter.svg"> 
+                </div>
+                <div class="textblock">
+                    <div class="title">
+                        Donkey Clothing
+                    </div>
+                    <div class="text">
+                        Donkey har kläder till barn, från nyfödd och baby, till barn i förskole-och skolåldern. Barnkläder och bebiskläder som underlättar vardagen och som fungerar lika bra för fartfyllda utomhusaktiviteter som till fest. Med 100 % hållbarhetsmärkt bomull i hela vår kollektion gör vi kläder till baby och barn, men också kläder till den nyfödda familjemedlemmen. Helt enkelt kläder som låter barn vara barn.
+                    </div>
+                </div>
+                <div class="infoblock">
+                    <div class="adress">
+                        Donkey Clothing<br>
+                        194 61 Stockholm<br>
+                        Sweden<br>
+                    </div>
+                    <div class="small-line1">
+                    </div>
+                    <div class="number">
+                        +46 0 000 000 00
+                    </div>
+                    <div class="small-line2">
+                    </div>
+                    <div class="mail">
+                        info@donkey.com
+                    </div>
+                </div>
+                <div class="line4">
+                </div>
+                <div class="linkblock">
+                    <div class="link-text">
+                        Om donkey
+                    </div>
+                    <div class="link-text">
+                        Hitta våra butiker
+                    </div>
+                    <div class="link-text">
+                        Jobba hos oss
+                    </div>
+                    <div class="link-text">
+                        Press
+                    </div>
+                    <div class="link-text">
+                        Privacy Policy
+                    </div>
+                    <div class="link-text">
+                        Cookie Policy
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+</body>
+</html>
